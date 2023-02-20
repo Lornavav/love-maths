97de01a90ded8d1e867a8ed74c52f62ed80ecb39
Lornavav <lornavavasour@yahoo.ie>
2023-02-20 07:41:18 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(params) {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculatewCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displauAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
